265b9e9b54b7f6bcb10274b481883800e4b09955
Emil <emil.levin125@gmail.com>
2024-01-01 22:08:32 +0400

    [Script] Kane sort of moves depending on track seen

--- Assets/Scripts/AI Systems/Kane/KaneSystem.cs
+++ Assets/Scripts/AI Systems/Kane/KaneSystem.cs
@@ -1,63 +1,91 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class KaneSystem : MonoBehaviour
 {   
+    public int rowCount = 3;
     public int visionRaysCount = 4;
     public float halfFieldOfView = 45f;
-    public float visionDistance = 5f;
-    private Vector3[] mainVisionRays;
-
+    public float maxVisionDistance = -3f;
+    public float rowIncrement = 0.5f;
+    public float rayHeight = 5f;
+    [SerializeField]
+    private Quaternion[] mainVisionRays;
+    [SerializeField]
+    private float steeringAngle = 0f;
+    // how fast does the steering go back to 0
+    // prevents the car having sporadic movement on uneven roads
+    public float steeringCenterRate = 0.8f;
     void Start()
     {
+        if (visionRaysCount % 2 != 0)
+            Debug.LogError("Should have an even amount of rays");
         mainVisionRays = CalculateRays();
     }
 
     void Update()
     {
-        #if UNITY_EDITOR
         mainVisionRays = CalculateRays();
-        #endif
-        DrawRays();
+        ControlCar();
     }
 
-    Vector3[] CalculateRays()
+    // starting left, first half points left, second half points right 
+    Quaternion[] CalculateRays()
     {
-        List<Vector3> rays = new List<Vector3>();
-
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // BUGGED - as car rotates around y axis, the spread of the vertical rays change
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        List<Quaternion> rays = new List<Quaternion>();
         for (int i = 0; i < visionRaysCount; i++)
         {
             float angle = i * (halfFieldOfView * 2f / (visionRaysCount - 1)) - halfFieldOfView; // Calculate angle within the specified range
-            Vector3 rayDirection = Quaternion.Euler(-visionDistance, angle, 0) * transform.forward; // Rotate the forward vector by the angle
-            
-            rays.Add(rayDirection);
+            for (int j = 0; j < rowCount; j++)
+            {
+                Quaternion rayDirection = Quaternion.Euler(-maxVisionDistance - rowIncrement * j * j, angle, 0);
+                rays.Add(rayDirection);
+            }
         }
         return rays.ToArray();
     }
 
-    void DrawRays()
-    {
 
-        foreach(Vector3 rayDirection in mainVisionRays)
+    void ControlCar()
+    {
+        for (int i = 0; i < mainVisionRays.Length; i++)
         {
+            Vector3 rayDirection = mainVisionRays[i] * transform.forward;
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, rayDirection, out hit))
+            Vector3 rayOrigin = transform.position + transform.up * rayHeight;
+            if (Physics.Raycast(rayOrigin, rayDirection, out hit))
             {
-                // Draw the ray until it hits an object
-                if (hit.collider.tag == "road")
-                    Debug.DrawRay(transform.position, rayDirection * hit.distance, Color.green);
+                if (hit.collider.tag != "road")
+                {
+                    if (i < mainVisionRays.Length / 2)
+                    {
+                        Debug.DrawRay(rayOrigin, rayDirection * hit.distance, Color.red);   
+                        Debug.Log(1f / ((float)mainVisionRays.Length / 2f));
+                        steeringAngle += 0.5f / ((float)mainVisionRays.Length / 2f);
+                    }    
+                    else
+                    {
+                        Debug.DrawRay(rayOrigin, rayDirection * hit.distance, Color.blue);   
+                        steeringAngle -= 0.5f / ((float)mainVisionRays.Length / 2f);
+                    }
+                }
                 else
-                    Debug.DrawRay(transform.position, rayDirection * hit.distance, Color.red);
+                {
+                    Debug.DrawRay(rayOrigin, rayDirection * hit.distance, Color.green);   
+                }
             }
             else
-            {
-                // Draw the ray if it doesn't hit anything
-                Debug.DrawRay(transform.position, rayDirection * visionDistance, Color.red);
-            }
+                Debug.DrawRay(rayOrigin, rayDirection * 100, Color.black);   
         }
+        CarController.Instance.SetSteeringInput(steeringAngle);
+
+        steeringAngle *= steeringCenterRate;
+        if (Mathf.Abs(steeringAngle) < 0.05f)
+            steeringAngle = 0;
     }
-
-
 }
