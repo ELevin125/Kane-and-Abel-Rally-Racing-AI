979a3fc9ab2bc195f7835168747eba6d8ad87ef9
Emil <emil.levin125@gmail.com>
2024-01-01 20:38:30 +0400

    [Script] calculate and store rays at start and added vertical angle to the rays

--- Assets/Scripts/AI Systems/Kane/KaneSystem.cs
+++ Assets/Scripts/AI Systems/Kane/KaneSystem.cs
@@ -5,29 +5,51 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class KaneSystem : MonoBehaviour
 {   
-    public int VisionRaysCount = 8;
+    public int visionRaysCount = 4;
     public float halfFieldOfView = 45f;
     public float visionDistance = 5f;
-    private float[] mainVisionRays;
+    private Vector3[] mainVisionRays;
+
+    void Start()
+    {
+        mainVisionRays = CalculateRays();
+    }
 
     void Update()
     {
+        #if UNITY_EDITOR
+        mainVisionRays = CalculateRays();
+        #endif
         DrawRays();
+    }
+
+    Vector3[] CalculateRays()
+    {
+        List<Vector3> rays = new List<Vector3>();
+
+        for (int i = 0; i < visionRaysCount; i++)
+        {
+            float angle = i * (halfFieldOfView * 2f / (visionRaysCount - 1)) - halfFieldOfView; // Calculate angle within the specified range
+            Vector3 rayDirection = Quaternion.Euler(-visionDistance, angle, 0) * transform.forward; // Rotate the forward vector by the angle
+            
+            rays.Add(rayDirection);
+        }
+        return rays.ToArray();
     }
 
     void DrawRays()
     {
 
-        for (int i = 0; i < VisionRaysCount; i++)
+        foreach(Vector3 rayDirection in mainVisionRays)
         {
-            float angle = i * (halfFieldOfView * 2f / (VisionRaysCount - 1)) - halfFieldOfView; // Calculate angle within the specified range
-            Vector3 rayDirection = Quaternion.Euler(0, angle, 0) * transform.forward; // Rotate the forward vector by the angle
-
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, rayDirection, out hit, visionDistance))
+            if (Physics.Raycast(transform.position, rayDirection, out hit))
             {
                 // Draw the ray until it hits an object
-                Debug.DrawRay(transform.position, rayDirection * hit.distance, Color.red);
+                if (hit.collider.tag == "road")
+                    Debug.DrawRay(transform.position, rayDirection * hit.distance, Color.green);
+                else
+                    Debug.DrawRay(transform.position, rayDirection * hit.distance, Color.red);
             }
             else
             {
