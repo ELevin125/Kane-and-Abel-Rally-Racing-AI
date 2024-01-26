ba8e926975cdbccf5b38407ea642a9f969310d8f
Emil <emil.levin125@gmail.com>
2024-01-26 16:50:01 +0400

    [Script] Stabilised Sensors on Abel

--- Assets/StabiliseSensors.cs
+++ Assets/StabiliseSensors.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class StabiliseSensors : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float raycastLength = 10f;
+    public float raycastAngle = 45f;
+    public float yOffset;
+    [Range(0f, 2f)]
+    public float tiltMultiplier = 1f;
+    public Transform targetTransform;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 rayDirection = Quaternion.AngleAxis(raycastAngle, transform.right) * transform.forward;
+        Ray ray = new Ray(transform.position + transform.up * yOffset, rayDirection);
+
+        // Draw the ray for visualization
+        Debug.DrawRay(ray.origin, ray.direction * raycastLength, Color.magenta);
+
+        RaycastHit hit;
+
+
+        if (Physics.Raycast(ray, out hit, raycastLength))
+        {
+            // Calculate the angle of the hit object
+            Debug.DrawRay(hit.point, hit.normal * 5, Color.black);
+            float slopeAngle = Vector3.Angle(hit.normal, Vector3.up);
+
+            // Calculate the rotation from the current up direction to the hit normal
+            Quaternion targetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+
+            // Apply the tilt rotation with the specified multiplier
+            float rotationAmount = slopeAngle * tiltMultiplier;
+            targetRotation *= Quaternion.Euler(rotationAmount, 0, 0);
+            targetTransform.rotation = targetRotation;
+        }
     }
 }
